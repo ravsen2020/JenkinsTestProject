7be33ae2de42124e324012a118b9d27012c86aee
ravsen2020 <69644186+ravsen2020@users.noreply.github.com>
2022-03-22 23:47:02 -0500

    Add files via upload

---
+++ FirstJavaPgm.java
@@ -0,0 +1,10 @@
+public class FirstJavaPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vetrivel,   Veeravel");
+		System.out.println("Sakthivel,   Vairavel");
+
+	}
+
+}
