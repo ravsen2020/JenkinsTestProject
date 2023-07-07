adf95ac9e8a2a9aeb0ee4e218b0f23d2577ae0ee
ravsen2020 <senthilkumar.venu@gmail.com>
2023-07-06 23:05:57 -0500

    Last final commir_2

--- FirstJavaPgm.java
+++ FirstJavaPgm.java
@@ -5,7 +5,7 @@ public class FirstJavaPgm {
 		System.out.println("Vetrivel,   Veeravel");
 		System.out.println("Sakthivel,   Vairavel");
 		System.out.println("Palani,   Aandava");
-		System.out.println("changes from eclipse");
+		System.out.println("changes from eclipse   zzzzzzzzzzzzz");
 //kutapaya  6-7-2023///////// today last update
 	}
 
