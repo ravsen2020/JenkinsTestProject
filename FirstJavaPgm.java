22b1cd6bccb2f83720b5a44b3add09eb6d8859db
ravsen2020 <senthilkumar.venu@gmail.com>
2023-07-09 20:33:14 -0500

    bug fix commit

--- FirstJavaPgm.java
+++ FirstJavaPgm.java
@@ -8,19 +8,19 @@
 
 		System.out.println("changes from eclipse   zzzzzzzzzzzzz");
 //kutapaya  6-7-2023///////// today last update    hfghfghfghf  update 9-7-2023 **** git update llllllll
-<<<<<<< HEAD
+
 	//new updated code from eclipse,     now fron Git
-=======
+
 	//new updated code from eclipse
 
 		System.out.println("First Branch Changes");
 		System.out.println("your Branches change1");
 		System.out.println("Active Branches changes 2");
-		System.out.println("final changes in branch code cvc");
+		System.out.println("final changes in branch code cvc  bug fix ");
 		
 
 
->>>>>>> 931a349e74329b19d80042e5f324a65b509a6486
+
 	}
 
 }
